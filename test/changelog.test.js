6bfa181895352a6071622099f5482c7df397eec2
Cody <ken51717@gmail.com>
2024-01-03 08:45:30 +0800

    fix :test會清空版本問題

--- test/changelog.test.js
+++ test/changelog.test.js
@@ -16,7 +16,7 @@ function testInitChangelog() {
     assert.strictEqual(fs.existsSync(changelogFile), true, 'Changelog should be created');
 }
 
-// 測試添加條目
+// 測試添加項目
 function testAddEntry() {
     const testMessage = "Test entry";
     changelog.addEntry(testMessage);
@@ -26,13 +26,31 @@ function testAddEntry() {
 }
 // 測試標記changelog為已釋出
 function testReleaseChangelog() {
-    // 初始化changelog以確保測試一致性
+    // 初始化changelog以確保測試一緻性
     changelog.initChangelog();
     changelog.releaseChangelog();
 
     const content = fs.readFileSync(changelogFile, { encoding: 'utf-8' });
     assert.match(content, /## Released on \d{4}-\d{2}-\d{2}/, 'Changelog should contain a release header');
 }
+
+// 備份 CHANGELOG.txt 文件
+function backupChangelog() {
+    if (fs.existsSync(changelogFile)) {
+        fs.copyFileSync(changelogFile, backupFile);
+    }
+}
+
+// 恢複 CHANGELOG.txt 文件
+function restoreChangelog() {
+    if (fs.existsSync(backupFile)) {
+        fs.renameSync(backupFile, changelogFile);
+    } else if (fs.existsSync(changelogFile)) {
+        // 如果測試期間創建了 CHANGELOG.txt 但冇有備份文件，則刪除它
+        fs.unlinkSync(changelogFile);
+    }
+}
+
 
 // 測試添加版本
 function testAddVersion() {
@@ -53,8 +71,7 @@ function testAddVersion() {
 
 // 測試顯示changelog
 function testShowChangelog() {
-    // 此功能可能需要更複雜的測試，因為它涉及到控制台輸出
-    // 您可能需要使用像是 sinon 這樣的庫來攔截和檢查console.log的調用
+    // 此功能可能需要更複雜的測試，因為它涉及到控製臺輸出
     console.log("testShowChangelog needs to be implemented");
 }
 
@@ -66,12 +83,16 @@ function testListVersions() {
 
 // 執行所有測試
 function runTests() {
+    backupChangelog(); // 備份原始 CHANGELOG.txt 文件
+
     testInitChangelog();
     testAddEntry();
     testReleaseChangelog();
     testAddVersion();
     // testShowChangelog(); // Uncomment after implementation
     // testListVersions(); // Uncomment after implementation
+
+    restoreChangelog(); // 恢複原始 CHANGELOG.txt 文件
 
     console.log("All tests passed!");
 }
